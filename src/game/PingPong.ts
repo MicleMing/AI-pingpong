87bbaef6fdeee244d88b2033bb76e5cfab0e8d0c
MicleMing <fzulanming@gmail.com>
2019-07-13 14:18:36 +0800

    feat: game

--- src/game/PingPong.ts
+++ src/game/PingPong.ts
@@ -13,7 +13,7 @@ interface IBall {
 export default class PingPong {
   presskeys: Object = {};
   ball: IBall = {
-    speed: 5,
+    speed: 3,
     x: 150,
     y: 100,
     width: 20,
@@ -21,24 +21,16 @@ export default class PingPong {
     directionY: 1
   };
   timer: any = null;
-  start: boolean = false;
-
-  score = {
-    scoreA: 0,
-    scoreB: 0
-  }
 
   play() {
-    $("#paddleA").css("top", "70px");
-    $("#paddleB").css("top", "100px");
+    $("#paddle").css("top", "100px");
     this.game();
   }
 
 
   game() {
-    this.timer = setInterval(this.gameloop.bind(this), 30);
+    this.timer = setInterval(this.gameloop.bind(this), 10);
     const presskeys = this.presskeys;
-    const self = this;
     $(document).keydown(function (e) {
       presskeys[e.which] = true;
     });
@@ -54,28 +46,16 @@ export default class PingPong {
 
   movePaddles() {
     const paddleTop = parseInt($("#playground").css("height")) - parseInt($(".paddle").css("height"));
-    if (this.presskeys[key.w]) {
-      const top = parseInt($("#paddleA").css("top"));
-      if (top > 0) {
-        $("#paddleA").css("top", top - 5);
-      }
-    }
-    if (this.presskeys[key.s]) {
-      const down = parseInt($("#paddleA").css("top"));
-      if (down < paddleTop) {
-        $("#paddleA").css("top", down + 5);
-      }
-    }
     if (this.presskeys[key.up]) {
-      const top = parseInt($("#paddleB").css("top"));
+      const top = parseInt($("#paddle").css("top"));
       if (top > 0) {
-        $("#paddleB").css("top", top - 5);
+        $("#paddle").css("top", top - 5);
       }
     }
     if (this.presskeys[key.down]) {
-      const down = parseInt($("#paddleB").css("top"));
+      const down = parseInt($("#paddle").css("top"));
       if (down < paddleTop) {
-        $("#paddleB").css("top", down + 5);
+        $("#paddle").css("top", down + 5);
       }
     }
   }
@@ -83,43 +63,28 @@ export default class PingPong {
   moveBall() {
     const playwidth = parseInt($("#playground").css("width"));
     const playheight = parseInt($("#playground").css("height"));
-    const paddleAleft = parseInt($("#paddleA").css("left")) + parseInt($("#paddleA").css("width"));
-    const paddleBleft = parseInt($("#paddleB").css("left"));
-    const paddleAtop = parseInt($("#paddleA").css("top"));
-    const paddleBtop = parseInt($("#paddleB").css("top"))
-    const paddleAbottom = parseInt($("#paddleA").css("top")) + parseInt($("#paddleA").css("height"));
-    const paddleBbottom = parseInt($("#paddleB").css("top")) + parseInt($("#paddleB").css("height"));
+    const paddleLeft = parseInt($("#paddle").css("left"));
+    const paddleTop = parseInt($("#paddle").css("top"))
+    const paddleBottom = parseInt($("#paddle").css("top")) + parseInt($("#paddle").css("height"));
     const ballLeft = this.ball.x + this.ball.speed * this.ball.directionX;
     const ballTop = this.ball.y + this.ball.speed * this.ball.directionY;
-    //A win
+    // Lost and restart
     if (ballLeft + this.ball.width >= playwidth) {
       this.ball.x = 300;
       this.ball.y = 150;
       $("#ball").css({ "top": this.ball.y, "left": this.ball.x });
       this.ball.directionX *= 1;
-      this.score.scoreA++;
-      $("#A").html("A:" + this.score.scoreA);
-    }
-    //B win
-    if (ballLeft <= 0) {
-      this.ball.x = 300;
-      this.ball.y = 150;
-      $("#ball").css({ "top": this.ball.y, "left": this.ball.x });
-      this.ball.directionX *= -1;
-      this.score.scoreB++;
-      $("#B").html("B:" + this.score.scoreB);
     }
 
     if (ballTop <= 0 || ballTop + this.ball.width >= playheight) {
       this.ball.directionY *= -1;
     }
-    if (ballLeft <= paddleAleft) {
-      if (ballTop >= paddleAtop && ballTop <= paddleAbottom) {
-        this.ball.directionX = 1;
-      }
+
+    if (ballLeft + this.ball.width <= 0) {
+      this.ball.directionX = 1;
     }
-    if (ballLeft + this.ball.width >= paddleBleft) {
-      if (ballTop >= paddleBtop && ballTop <= paddleBbottom) {
+    if (ballLeft + this.ball.width >= paddleLeft) {
+      if (ballTop >= paddleTop && ballTop <= paddleBottom) {
         this.ball.directionX = -1;
       }
     }
